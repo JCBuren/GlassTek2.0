04deeef6eb21418738f03ea415adb04cd004737c
Jordan <JBuren1957@Gmail.com>
2023-04-09 23:10:53 -0500

    Updates to Replace

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,10 +10,17 @@ const Home = () => {
 			<div className="w-screen h-screen pb-24 mb-32">
 				<img src={HomeImage} alt="/" className="w-screen h-screen pb-24" />
 				<div className="left-1">
-					Welcome to Glasstek, where technology and technique meet. We
-					offer service on any vehicle in Sioux Falls and the surrounding
-					areas. We use the latest technology and tools to ensure a safe &
-					high quality product. We started Glasstek to....
+					Welcome to Glasstek! We offer Auto-Glass service on any vehicle
+					in Sioux Falls and the surrounding areas. Glasstek will work with{" "}
+					<span className="font-bold">EVERY</span> insurance company for
+					your Glass Replacement or Chip Repair, and we can give you a
+					free, no obligation quote HERE. Our state of the art calibration
+					equipment can help keep your cars' ADAS Systems (Lane Depature
+					Warning, Lane Keep Assist, Eyesight) working properly. We also
+					specialize in Auto Glass servicing for Commerical fleets, call us
+					to ask how we can help keep your equipment on-site. We are
+					passionite about Auto Glass and you'll see it in our
+					work.
 				</div>
 			</div>
 		</div>
--- src/components/Replace.jsx
+++ src/components/Replace.jsx
@@ -1,14 +1,29 @@
 import React from "react"
 import NavBar from "./NavBar"
+import ReplacePic from "../assets/Replace.jpg"
 
 const Replace = () => {
-	return(
-<>
-    <NavBar/>
-
-    <div>About</div>
-    
-    </>
+	return (
+		<>
+			<div className="flex">
+				<NavBar />
+				<div className="p-3 max-h-full w-[400px]">
+					<h1 className="p-6 font-bold text-xl">Auto Glass Replacement</h1>
+					<a className=" inherit align-middle">
+						Windshields, Backglass, Doors, Side Windows, we do it all!
+						We'll work on anything from your daily driver to brand new or
+						custom hotrods. We'll also take great care of your classic
+						car, or that project car in your garage. Our dedication to
+						excellence means even if we find rust, we'll treat it for free
+						as long as the glass is safe to replace. We prioritize safety, so all vehicles with ADAS (Lane Depature
+					Warning, Lane Keep Assist, Eyesight) are calibrated as part of your quote. 
+					</a>
+					<button className="flex p-8 mt-6 ml-14"> Click here for a Quote</button>
+				</div>
+				<img src={ReplacePic} alt="/" className="w-[1420px] h-[942px]" />
+				
+			</div>
+		</>
 	)
 }
 
