cd518b08295f81a2a45be97de1001223804dc311
Jordan <JBuren1957@Gmail.com>
2023-04-15 21:20:16 -0500

    deploy fix

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,7 +8,8 @@ const Home = () => {
 		<div className="flex max-h-screen">
 			<NavBar />
 			<div className="w-screen ">
-				<h1 className="absolute bottom-20 text-my-white font-bold text-xl p-2 text-center shadow-light-white">Welcome to Glasstek! We offer Auto-Glass service on any vehicle
+				<h1 className="absolute bottom-20 text-my-white font-bold text-xl p-2 text-center shadow-light-white">
+					Welcome to Glasstek! We offer Auto-Glass service on any vehicle
 					in Sioux Falls and the surrounding areas. Glasstek will work with{" "}
 					<span className="font-bold">EVERY</span> insurance company for
 					your Glass Replacement or Chip Repair, and we can give you a
@@ -17,11 +18,9 @@ const Home = () => {
 					Warning, Lane Keep Assist, Eyesight) working properly. We also
 					specialize in Auto Glass servicing for Commerical fleets, call us
 					to ask how we can help keep your equipment on-site. We are
-					passionite about Auto Glass and you'll see it in our work.</h1>
-				<img src={HomeImage} alt="/" className=" h- " />
-				
-					
-				
+					passionite about Auto Glass and you'll see it in our work.
+				</h1>
+				<img src={HomeImage} alt="/" className="" />
 			</div>
 		</div>
 	)
